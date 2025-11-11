1ba1a6ba2a6abf44888dc58a0644c12f0347fd60
mynte02 <mariaastrup02@hotmail.com>
2025-11-11 14:13:32 +0100

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="space-y-3 text-center">
-      <h1 className="text-xl font-semibold">Velkommen til Session Planner</h1>
-      <p>Log ind for at fortsætte.</p>
-      <div className="flex gap-2 text-center justify-center">
-        <Link className="underline" href="/sign-in">Log ind</Link>
-        <Link className="underline" href="/(student)/page">Se sessions</Link>
+    <div className="bg-gray-100 bg-cover bg-center min-h-screen flex items-center justify-center">
+      <div className="space-y-3 text-center bg-white shadow-lg rounded-lg p-8 max-w-md mx-auto">
+        <h1 className="text-2xl font-semibold">Velkommen til Session Planner</h1>
+        <p>Log ind for at fortsætte.</p>
+        <div className="flex gap-2 text-center justify-center">
+          <Link className="underline" href="/sign-in">Log ind</Link>
+          <Link className="underline" href="/(student)/page">Se sessions</Link>
+        </div>
       </div>
     </div>
   );
--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -3,10 +3,19 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import AuthModal from "@/src/components/AuthModal";
+import { signIn } from "@/src/lib/auth";
 
 export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
   const [open, setOpen] = useState(true); // åbner med det samme
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+  }
 
   useEffect(() => {
     if (!open) router.push("/"); // luk → tilbage til forsiden
